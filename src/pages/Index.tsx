ac81d41f1c3651662decb8fbad4e500618389479
Alex Strive <alexstrive@pm.me>
2022-09-28 21:45:43 +0300

    refactor: Index page simplification

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,5 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 export default function Index() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const user = null;
-
-    if (!user) {
-      navigate('/sign-in');
-    }
-  });
-
-  return <div>Wait for redirection</div>;
+  return <Navigate to="/sign-in" />;
 }
